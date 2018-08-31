cc2b57293de1b1d3446a4116bc1e11c9933b6901
bingqixuan <bingqixuan@vip.qq.com>
2018-08-31 10:05:15 +0800

    finish 地形淹没分析

--- Source/Shaders/Materials/ElevationRampMaterial.glsl
+++ Source/Shaders/Materials/ElevationRampMaterial.glsl
@@ -1,6 +1,7 @@
 uniform sampler2D image;
 uniform float minimumHeight;
 uniform float maximumHeight;
+uniform float visualHeight;
 
 czm_material czm_getMaterial(czm_materialInput materialInput)
 {
@@ -8,6 +9,10 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
     float scaledHeight = clamp((materialInput.height - minimumHeight) / (maximumHeight - minimumHeight), 0.0, 1.0);
     vec4 rampColor = texture2D(image, vec2(scaledHeight, 0.5));
     material.diffuse = rampColor.rgb;
-    material.alpha = rampColor.a;
+    if(materialInput.height > visualHeight){
+        material.alpha = 0.0;
+    }else{
+        material.alpha = rampColor.a;
+    }
     return material;
 }
